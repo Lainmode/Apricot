dceced6fbb9bb4bc6084be011f2607f9506f06c1
SaishoAlita <44531827+SaishoAlita@users.noreply.github.com>
2022-12-21 15:31:53 +0300

    added layout page and bootstrapp sass and made homepage a lil

--- Apricot/Controllers/HomeController.cs
+++ Apricot/Controllers/HomeController.cs
@@ -15,11 +15,45 @@ namespace Apricot.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.CurrentPage = CurrentPage.Home;
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            ViewBag.CurrentPage = CurrentPage.SignUp;
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            ViewBag.CurrentPage = CurrentPage.Login;
+            return View();
+        }
+
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult Chat(int chatId)
+        {
+            return View();
+        }
+
+        public IActionResult Space(int spaceId)
+        {
             return View();
         }
 
         public IActionResult Privacy()
         {
+            ViewBag.CurrentPage = CurrentPage.About;
+            return View();
+        }
+        public IActionResult ContactSupport()
+        {
+            ViewBag.CurrentPage = CurrentPage.ContactSupport;
             return View();
         }
 
---
+++ Apricot/Models/Common.cs
@@ -0,0 +1,17 @@
+﻿namespace Apricot.Models
+{
+    public class Common
+    {
+    }
+
+    public enum CurrentPage
+    {
+        None = 0,
+        Home = 1,
+        Login = 2,
+        SignUp = 3,
+        About = 4,
+        ContactSupport = 5,
+
+    }
+}
